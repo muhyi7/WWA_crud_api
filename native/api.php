109da86e1b0b38990dcf8f3c036238b0ccc3c14d
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-01 13:52:45 +0700

    function update dan delete

--- native/api.php
+++ native/api.php
@@ -81,4 +81,74 @@ function get_user_id()
     header('Content-Type: application/json');
     echo json_encode($response);
 }
+function update_user()
+{
+    global $koneksi;
+    if (!empty($_GET["id"])) {
+        $id = $_GET["id"];
+        $email = $_GET["email"];
+        $password = $_GET["password"];
+        $name = $_GET["name"];
 
+        // Gunakan pernyataan bersiap-siap dengan PDO
+        $query = $koneksi->prepare("UPDATE user_detail SET user_email = :email, user_password = :password, user_fullname = :name WHERE id = :id");
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':password', $password, PDO::PARAM_STR);
+        $query->bindParam(':name', $name, PDO::PARAM_STR);
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $result = $query->execute();
+
+        if ($result) {
+            $response = array(
+                'status' => 1,
+                'message' => 'Update Success'
+            );
+        } else {
+            $response = array(
+                'status' => 0,
+                'message' => 'Update Failed'
+            );
+        }
+    } else {
+        $response = array(
+            'status' => 0,
+            'message' => 'Missing ID'
+        );
+    }
+
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function delete_user()
+{
+    global $koneksi;
+    if (!empty($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = $koneksi->prepare("DELETE FROM user_detail WHERE id = :id");
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $result = $query->execute();
+
+        if ($result) {
+            $response = array(
+                'status' => 1,
+                'message' => 'Delete Success'
+            );
+        } else {
+            $response = array(
+                'status' => 0,
+                'message' => 'Delete Failed'
+            );
+        }
+    } else {
+        $response = array(
+            'status' => 0,
+            'message' => 'Missing ID');
+        }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+?>
